e3530ee390f0a438f91f9bbfcff7a5be6e0cfb9c
Stefan <stepson_jonsson@hotmail.com>
2022-07-08 23:57:17 +0200

    Startar spel med steams gameId istället för exe fil!

--- RandomGameStarter/Program.cs
+++ RandomGameStarter/Program.cs
@@ -1,33 +1,41 @@
-﻿string startFolder = @"E:\SteamLibrary\steamapps\common";
+﻿string startFolder = @"E:\SteamLibrary\steamapps";
 // Ändra startmappen för att passa din dator.
 
 System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(startFolder);
 IEnumerable<System.IO.FileInfo> fileList = dir.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
-string searchTerm = @""; // Om du vill filtrera efter filnamn kommentera ut "where"
-
-
 
 var queryMatchingFiles =
     from file in fileList
-    where file.Extension == ".exe"
+    where file.Extension == ".acf"
     let fileText = GetFileText(file.DirectoryName)
-    //where fileText.Contains(searchTerm)
     select file.FullName;
 
 List<string> games = new List<string>(); // Skapar en tom lista.
 
 foreach (var filename in queryMatchingFiles) 
 {
-    games.Add(filename); // Stoppar in alla file paths i en lista.
+    games.Add(filename); // Stoppar in alla appmanifest.acf filer i listan.
 
 }
-var random = new Random();
-int randomGame = random.Next(games.Count); // Slumpar ett spels filepath ur listan.
-Console.WriteLine(games[randomGame]);
-System.Diagnostics.Process.Start(games[randomGame]); // Startar den exe filen vi slumpat fram.
+List<string> gameIds = new List<string>(); // Skapar ny lista för att hålla alla game Ids.
 
-     
-    static string GetFileText(string name)
+foreach (var game in games)
+{
+    var test = game.Substring(game.IndexOf('_') +1); 
+    var test2 = test.Remove(test.IndexOf('.'));   
+    gameIds.Add(test2);
+    // Stränghantering, t.ex "appmanifest_205100.acf" blir "205100", alltså gameId.
+}
+
+var random = new Random();
+int randomGame = random.Next(gameIds.Count); // Slumpar ett spels gameid ur listan.
+Console.WriteLine("Startar spelet med gameid: " + gameIds[randomGame]);
+
+string cmdText = "/K D:&cd Steam& steam steam://run/" + gameIds[randomGame];
+System.Diagnostics.Process.Start("CMD.exe", cmdText);
+//Startar CMD och startar spelet med det slumpade game:id
+
+static string GetFileText(string name)
     {
         string fileContents = String.Empty;
 
